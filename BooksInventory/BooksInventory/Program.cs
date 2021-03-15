4178cf7c5fba9280afc5673ffaca26ca4df55cff
jwright7041 <jmwright120@gmail.com>
2021-03-14 19:32:29 -0500

    BooksInventory

---
+++ BooksInventory/BooksInventory/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+
+namespace BooksInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an instance of the context
+            StudentsContext context = new StudentsContext();
+
+            // makes sure that the table exists, 
+            //and creates it if it does not already exist
+            context.Database.EnsureCreated();
+
+            // ask the user for a student to add
+            Console.WriteLine("Enter Student full name");
+            String fullName = Console.ReadLine();
+
+            // split the input into parts, and make sure 
+            // we have 2 parts only
+            String[] parts = fullName.Split();
+            if (parts.Length == 2)
+            {
+                // create a new student object, notice that we do not 
+                // select an id, we let the framework handle that
+                Student newStudent = new Student(parts[0], parts[1]);
+
+                // add the newly created student instance to the context
+                // notice how similar this is to adding a item to a list,
+                context.students.Add(newStudent);
+
+                // ask the context to save any changes to the database 
+                context.SaveChanges();
+                Console.WriteLine("Added the student.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid full name, did not add student");
+            }
+
+            Console.WriteLine("The Current List of students are: ");
+            // use a for each loop to loop through the students in the context
+            // notice how similar this is to looping through a list
+            foreach (Student s in context.students)
+            {
+                Console.WriteLine("{0} - {1} {2}",
+                     s.Id, s.FirstName, s.LastName);
+            }
+        }
+    }
+
+    class Student
+    {
+        // notice the private set on the id
+        public int Id { get; private set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public Student(String FirstName, String LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+
+    class StudentsContext : DbContext
+    {
+
+        // This property corresponds to the table in our database
+        public DbSet<Student> students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            // add 'students.db' to the project directory
+            String DatabaseFile = Path.Combine(ProjectBase.FullName, "students.db");
+
+            // to check what the path of the file is, uncomment the file below
+            //Console.WriteLine("using database file :"+DatabaseFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+        }
+    }
+}
---
+++ BooksInventory/BooksInventory2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+
+namespace BooksInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an instance of the context
+            BookContext context = new BookContext();
+
+            // makes sure that the table exists, 
+            //and creates it if it does not already exist
+            context.Database.EnsureCreated();
+
+            // ask the user for a title and author to add
+            Console.WriteLine("Enter book title");
+            String title = Console.ReadLine();
+
+            Console.WriteLine("Enter author name");
+            String author = Console.ReadLine();
+            
+                // create a new book object, notice that we do not 
+                // select an id, we let the framework handle that
+                Book newBook = new Book(title, author);
+
+                // add the newly created book instance to the context
+                // notice how similar this is to adding a item to a list,
+                context.books.Add(newBook);
+
+                // ask the context to save any changes to the database 
+                context.SaveChanges();
+                Console.WriteLine("Added the book.");
+
+            
+
+            Console.WriteLine("The Current List of books are: ");
+            // use a for each loop to loop through the books in the context
+            // notice how similar this is to looping through a list
+            foreach (Book s in context.books)
+            {
+                Console.WriteLine("{0} : {1} - {2}",
+                     s.Id, s.Title, s.Name);
+            }
+        }
+    }
+
+    class Book
+    {
+        // notice the private set on the id
+        public int Id { get; private set; }
+        public String Title { get; set; }
+        public String Name { get; set; }
+        public Book(String Title, String Name)
+        {
+            this.Title = Title;
+            this.Name = Name;
+        }
+    }
+
+    class BookContext : DbContext
+    {
+
+        // This property corresponds to the table in our database
+        public DbSet<Book> books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            // add 'books.db' to the project directory
+            String DatabaseFile = Path.Combine(ProjectBase.FullName, "books.db");
+
+            // to check what the path of the file is, uncomment the file below
+            //Console.WriteLine("using database file :"+DatabaseFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+        }
+    }
+}
+
